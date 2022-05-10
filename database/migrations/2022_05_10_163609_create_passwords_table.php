0e9a367cd3324a294c463a345281d4789e1ef95e
bijaydas <imbijaydas@gmail.com>
2022-05-10 17:47:20 +0530

    (added) Store password structure
      1. Added password table.
      2. Added password model.
      3. Added password service.
      4. Added Url already exists exception

---
+++ database/migrations/2022_05_10_163609_create_passwords_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Password;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('passwords', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->timestamp('last_used')->nullable();
+            $table->string('url');
+            $table->string('name', 50)->nullable();
+            $table->string('password', 150);
+            $table->text('note')->nullable();
+            $table->tinyInteger('is_favourite')->default(Password::NOT_FAVOURITE);
+            $table->tinyInteger('report')->default(Password::NOT_CHECKED);
+            $table->string('logo')->default('logo.png');
+            $table->softDeletes();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('passwords');
+    }
+};
